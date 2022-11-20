bcac696c0d01c112d5c665a439c4f8377f477f4d
Burakrstem <114940951+Burakrstem@users.noreply.github.com>
2022-11-20 13:29:30 +0300

    Update 8.red_point.py

--- python_exercise/8.red_point.py
+++ python_exercise/8.red_point.py
@@ -3,6 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-img=cv2.imread("/home/brk2004/Desktop/python/first_task/red_point.png")
+img=cv2.imread("red_point.png")
 plt.figure(figsize=(20,8))
 plt.imshow(img)
